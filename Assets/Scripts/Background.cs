96567df8bfc03ec6397df8ac4a194877733778d2
voyageth <voyageth@gmail.com>
2024-05-11 17:24:35 +0900

    :sparkles: 무한 배경(B33)

---
+++ Assets/Scripts/Background.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Background : MonoBehaviour
+{
+    public float speed;
+    public int startIndex;
+    public int endIndex;
+    public Transform[] sprites;
+    
+    float viewHeight;
+
+    private void Awake()
+    {
+        viewHeight = Camera.main.orthographicSize * 2;
+    }
+
+    void Update()
+    {
+        Move();
+        Scrolling();
+    }
+
+    private void Move()
+    {
+        // move
+        Vector3 currentPosition = transform.position;
+        Vector3 nextPosition = Vector3.down * speed * Time.deltaTime;
+        transform.position = currentPosition + nextPosition;
+    }
+
+    private void Scrolling()
+    {
+        if (sprites[endIndex].position.y < -viewHeight)
+        {
+            // sprite reuse
+            Vector3 backSpritePosition = sprites[startIndex].localPosition;
+            Vector3 frontSpritePosition = sprites[endIndex].localPosition;
+            sprites[endIndex].transform.localPosition = backSpritePosition + Vector3.up * viewHeight;
+
+            // cursor index change
+            int startIndexSave = startIndex;
+            startIndex = endIndex;
+            endIndex = --startIndexSave;
+            if (endIndex <= -1)
+                endIndex = sprites.Length - 1;
+        }
+    }
+}
